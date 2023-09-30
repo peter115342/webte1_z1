b105495409906a9db19fe7fd26aaaf94258781f7
peter115342 <pmuzslay8@gmail.com>
2023-09-30 07:44:59 +0200

    changes

--- mzpdvducpudpjvnmsweqaetnh/app.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById("responsiveMenuButton");
-const toggleDropdown = document.getElementById("responsiveMenuButton");
-const dropMenu = document.querySelector(".dropdown");
-
- menuToggle.onclick = function() {
-     dropMenu.classList.toggle("open");
- }
